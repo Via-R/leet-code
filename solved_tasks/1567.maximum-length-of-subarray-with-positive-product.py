1a7883948e59bbb8ffcf08067618b7c9d939fc01
Vadym Rets <vadym@roundforest.com>
2024-02-18 00:01:55 +0200

    1567: remove recursion, optimise time

--- solved_tasks/1567.maximum-length-of-subarray-with-positive-product.py
+++ solved_tasks/1567.maximum-length-of-subarray-with-positive-product.py
@@ -2,74 +2,28 @@ from typing import List
 
 
 class Solution:
-    def get_possible_sub_array_limit(self, nums, from_idx):
-        # search for the first non-zero element
-        start_idx = from_idx
-        for idx, num in enumerate(nums[from_idx:]):
-            if num != 0:
-                start_idx = idx + from_idx
-                break
-        else:
-            raise Exception('No possible answer here')
-        # search for the next zero element (or the end of the array)
-        end_idx = None
-        for idx, num in enumerate(nums[start_idx:]):
-            if num == 0:
-                end_idx = idx + start_idx
-                break
-
-        end_idx = end_idx or len(nums)
-
-        return start_idx, end_idx
 
     def getMaxLen(self, nums: List[int]) -> int:
-        if not len(nums) or len(nums) == 1 and nums[0] <= 0:
-            return 0
-        if len(nums) == 1 and nums[0] > 0:
-            return 1
+        positive_product_length, negative_product_length = 0, 0
+        if nums[0] > 0:
+            positive_product_length = 1
+        elif nums[0] < 0:
+            negative_product_length = 1
 
-        valid_sub_arrays_limits = []
+        answer = positive_product_length
 
-        if 0 in nums:
-            try:
-                sub_array_start_idx, sub_array_end_idx = self.get_possible_sub_array_limit(nums, 0)
-            except:
-                return 0
+        for num_idx in range(1, len(nums)):
+            if nums[num_idx] > 0:
+                positive_product_length = positive_product_length + 1
+                negative_product_length = negative_product_length + 1 if negative_product_length > 0 else 0
+            elif nums[num_idx] < 0:
+                positive_product_length, negative_product_length = negative_product_length + 1 if negative_product_length > 0 else 0, positive_product_length + 1
+            else:
+                positive_product_length, negative_product_length = 0, 0
 
-            valid_sub_arrays_limits.append((sub_array_start_idx, sub_array_end_idx))
+            answer = max(answer, positive_product_length)
 
-            while sub_array_end_idx != len(nums):
-                try:
-                    sub_array_start_idx, sub_array_end_idx = self.get_possible_sub_array_limit(nums,
-                                                                                               sub_array_end_idx + 1)
-                    valid_sub_arrays_limits.append((sub_array_start_idx, sub_array_end_idx))
-                except:
-                    break
-
-        else:
-            valid_sub_arrays_limits = [(0, len(nums))]
-
-        sub_array_lens = [0] * len(valid_sub_arrays_limits)
-        for idx, (sub_array_start_idx, sub_array_end_idx) in enumerate(valid_sub_arrays_limits):
-            sub_nums = nums[sub_array_start_idx: sub_array_end_idx]
-            if sum([1 for num in sub_nums if num < 0]) % 2 == 0:
-                sub_array_lens[idx] = len(sub_nums)
-                continue
-
-            left_idx = next(i for i, v in enumerate(sub_nums) if v < 0)
-            mirrored_right_idx = next(i for i, v in enumerate(sub_nums[::-1]) if v < 0)
-            right_idx = len(sub_nums) - mirrored_right_idx - 1
-            if left_idx == right_idx:
-                sub_array_lens[idx] = max(self.getMaxLen(sub_nums[0:left_idx]), self.getMaxLen(sub_nums[left_idx + 1:]))
-                continue
-
-            if left_idx > mirrored_right_idx:
-                sub_array_lens[idx] = right_idx
-                continue
-
-            sub_array_lens[idx] = len(sub_nums) - left_idx - 1
-
-        return max(sub_array_lens)
+        return answer
 
 
 def main():
@@ -77,7 +31,6 @@ def main():
     print(s.getMaxLen([1, -2, -3, 4]))
     print(s.getMaxLen([0, 1, -2, -3, -4]))
     print(s.getMaxLen([-1, -2, -3, 0, 1]))
-    print(s.getMaxLen([0, 0, 0, 0, 0, 1, -1, 1, 0, 0, 0, 0, 1, 0, 0, -1, 1, -1, 1]))
 
 
 if __name__ == "__main__":
