892c3ad79ceee5a098411fba52206f03b4bd2b84
Vadym Rets <vadym@roundforest.com>
2024-02-21 23:34:33 +0200

    1362: change solution

--- solved_tasks/1362.closest-divisors.py
+++ solved_tasks/1362.closest-divisors.py
@@ -3,26 +3,28 @@ from math import sqrt
 
 
 class Solution:
-    def find_right_divisor(self, left_divisor, num):
-        right_divisor = left_divisor + 1
-        while left_divisor * right_divisor <= num + 2:
-            if left_divisor * right_divisor == num + 1 or left_divisor * right_divisor == num + 2:
-                return right_divisor
-            right_divisor += 1
+    def find_biggest_divisor(self, num, max_divisor=1):
+        divisor = int(sqrt(num)) + 1
+        if divisor ** 2 == num:
+            return [divisor, divisor]
+
+        while divisor >= (max_divisor or 1):
+            if num % divisor == 0:
+                return divisor
+            divisor -= 1
 
         return None
 
     def closestDivisors(self, num: int) -> List[int]:
-        middle = int(sqrt(num)) + 1
-        if middle ** 2 == num + 1 or middle ** 2 == num + 2:
-            return [middle, middle]
-
-        left_divisor = middle
-        while left_divisor >= 1:
-            right_divisor = self.find_right_divisor(left_divisor, num)
-            if right_divisor:
-                return [left_divisor, right_divisor]
-            left_divisor -= 1
+        first_divisor = self.find_biggest_divisor(num + 1)
+        second_divisor = self.find_biggest_divisor(num + 2, first_divisor)
+        bigger_divisor = max(first_divisor or -1, second_divisor or -1)
+        if bigger_divisor == -1:
+            return [-1, -1]
+        if first_divisor == bigger_divisor:
+            return [first_divisor, (num + 1) // first_divisor]
+        if second_divisor == bigger_divisor:
+            return [second_divisor, (num + 2) // second_divisor]
 
         return [-1, -1]
 
