a9822bdb52980a02f2eb0d9246225e9f8618bb59
Vadym Rets <vadym@roundforest.com>
2024-03-01 14:43:27 +0200

    1146: change solution to lists with pointers, O(1) for both get and set, O(n) for snap

--- solved_tasks/1146.snapshot-array.py
+++ solved_tasks/1146.snapshot-array.py
@@ -1,42 +1,29 @@
+from copy import copy
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.snapshots = []
-        self.current_array = [0] * length
-        self.changes_indexes = [[] for _ in range(length)]
+        self.snapshots = [[{"curr_val": 0, "saved_val": 0} for _ in range(length)]]
 
     def set(self, index: int, val: int) -> None:
-        self.current_array[index] = val
+        self.snapshots[-1][index]["curr_val"] = val
 
     def snap(self) -> int:
-        snapshot = [None] * len(self.current_array)
-        new_snap_id = len(self.snapshots)
-        for idx, n in enumerate(self.current_array):
-            if len(self.snapshots) and self.snapshots[-1][idx] == n:
-                continue
-            self.changes_indexes[idx].append(new_snap_id)
-            snapshot[idx] = n
-        self.snapshots.append(snapshot)
+        new_snapshot = []
+        for idx, data in enumerate(self.snapshots[-1]):
+            if data["curr_val"] != data["saved_val"]:
+                new_data = {"curr_val": data["curr_val"], "saved_val": data["curr_val"]}
+                self.snapshots[-1][idx] = new_data
+                new_snapshot.append(new_data)
 
-        return new_snap_id
+            new_snapshot.append(data)
+
+        self.snapshots.append(new_snapshot)
+
+        return len(self.snapshots) - 2
 
     def get(self, index: int, snap_id: int) -> int:
-        if self.snapshots[snap_id][index] is not None:
-            return self.snapshots[snap_id][index]
-
-        changes_indexes = self.changes_indexes[index]
-
-        if snap_id < changes_indexes[0]:
-            return 0
-
-        bs_bounds = (0, len(changes_indexes) - 1)
-        while bs_bounds[1] - bs_bounds[0] > 1:
-            middle_idx = len(bs_bounds) // 2
-
-            bs_bounds = (bs_bounds[0], middle_idx - 1) if snap_id < changes_indexes[middle_idx] else (
-            middle_idx, bs_bounds[1])
-
-        return self.snapshots[changes_indexes[bs_bounds[0]]][index]
+        print(self.snapshots)
+        return self.snapshots[snap_id][index]["saved_val"]
 
 
 def main():
@@ -46,16 +33,16 @@ def main():
     obj.set(0, 6)
     print('Get array[0] (snap=0): ', obj.get(0, 0))
 
-    # obj = SnapshotArray(1)
-    # obj.snap()
-    # obj.snap()
-    # obj.set(0, 4)
-    # obj.snap()
-    # print('Get array[0] (snap=1): ', obj.get(0, 1))
-    # obj.set(0, 12)
-    # print('Get array[0] (snap=1): ', obj.get(0, 1))
-    # obj.snap()
-    # print('Get array[0] (snap=3): ', obj.get(0, 3))
+    obj = SnapshotArray(1)
+    obj.snap()
+    obj.snap()
+    obj.set(0, 4)
+    obj.snap()
+    print('Get array[0] (snap=1): ', obj.get(0, 1))
+    obj.set(0, 12)
+    print('Get array[0] (snap=1): ', obj.get(0, 1))
+    obj.snap()
+    print('Get array[0] (snap=3): ', obj.get(0, 3))
 
 
 if __name__ == "__main__":
