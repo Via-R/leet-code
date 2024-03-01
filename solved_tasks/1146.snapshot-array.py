4bb8a3e07f7ce6c6c5965128eb86ceb57745942d
Vadym Rets <vadym@roundforest.com>
2024-03-01 16:34:14 +0200

    1146: add working solution

--- solved_tasks/1146.snapshot-array.py
+++ solved_tasks/1146.snapshot-array.py
@@ -1,34 +1,54 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.snapshots = [[{"val": 0}] * length]
-        self.length = length
+        self.array = [0] * length
+        self.snapshots = [{} for _ in range(length)]
+        self.change_ids = [[] for _ in range(length)]
         self.snapshot_count = 0
+        self.changed_idxs = set()
 
     def set(self, index: int, val: int) -> None:
-        snapshots_length = len(self.snapshots)
-        while self.snapshot_count >= snapshots_length:
-            new_snapshot = [n for n in self.snapshots[-1]]
-            self.snapshots.append(new_snapshot)
-            snapshots_length = len(self.snapshots)
-
-        if snapshots_length > 1 and val != self.snapshots[-2][index]["val"]:
-            self.snapshots[-1][index] = {"val": val}
-        elif snapshots_length == 1:
-            self.snapshots[-1][index]["val"] = val
+        change_ids = self.change_ids[index]
+        snapshots = self.snapshots[index]
+        last_val = snapshots[change_ids[-1]] if len(change_ids) else 0
+        if last_val != val:
+            self.changed_idxs.add(index)
         else:
-            self.snapshots[-1][index] = self.snapshots[-2][index]
+            self.changed_idxs.discard(index)
+
+        self.array[index] = val
 
     def snap(self) -> int:
+        for idx in self.changed_idxs:
+            self.change_ids[idx].append(self.snapshot_count)
+            self.snapshots[idx][self.change_ids[idx][-1]] = self.array[idx]
+
         self.snapshot_count += 1
+        self.changed_idxs = set()
 
         return self.snapshot_count - 1
 
     def get(self, index: int, snap_id: int) -> int:
-        if snap_id > len(self.snapshots) - 1:
-            return self.snapshots[-1][index]["val"]
+        change_ids = self.change_ids[index]
+        if not len(change_ids):
+            return 0
 
-        return self.snapshots[snap_id][index]["val"]
+        snapshots = self.snapshots[index]
+        if snapshots.get(snap_id) is not None:
+            return snapshots[snap_id]
+
+        left, right = 0, len(change_ids) - 1
+        while left < right:
+            middle = (left + right) // 2
+            if snap_id < change_ids[middle]:
+                right = middle - 1
+            else:
+                left = middle + 1
+
+        if snap_id < change_ids[left]:
+            return snapshots[change_ids[left - 1]] if left > 0 else 0
+
+        return snapshots[change_ids[left]]
 
 
 def main():
@@ -37,6 +57,9 @@ def main():
     obj.snap()
     obj.set(0, 6)
     print('Get array[0] (snap=0): ', obj.get(0, 0))
+    # must be 5
+
+    print()
 
     obj = SnapshotArray(1)
     obj.snap()
@@ -48,6 +71,24 @@ def main():
     print('Get array[0] (snap=1): ', obj.get(0, 1))
     obj.snap()
     print('Get array[0] (snap=3): ', obj.get(0, 3))
+    # must be 0 0 12
+
+    print()
+
+    obj = SnapshotArray(1)
+    obj.snap()
+    obj.snap()
+    obj.set(0, 4)
+    obj.snap()
+    obj.snap()
+    obj.set(0, 12)
+    obj.snap()
+    print('Get array[0] (snap=0): ', obj.get(0, 0))
+    print('Get array[0] (snap=1): ', obj.get(0, 1))
+    print('Get array[0] (snap=2): ', obj.get(0, 2))
+    print('Get array[0] (snap=3): ', obj.get(0, 3))
+    print('Get array[0] (snap=4): ', obj.get(0, 4))
+    #  must be 0 0 4 4 12
 
 
 if __name__ == "__main__":
